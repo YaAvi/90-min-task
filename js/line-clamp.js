50381d359c6360022fad38709fde3d57a978a858
Avi Y <aviyaakov1@gmail.com>
2016-09-06 15:36:18 +0300

    indentation fix

--- js/line-clamp.js
+++ js/line-clamp.js
@@ -1,52 +1,53 @@
 (function() {
-	'use strict';
-	var elements;
-	function polyfillElement(element) {
-		var textNode = element.children[0];
-		while (textNode.clientHeight > element.clientHeight) {
- 		   textNode.innerText = textNode.innerText.replace(/\W*\s(\S)*$/, '...');
-		}
-	}
+    'use strict';
+    var elements;
 
-	function reapply() {
-		for (var i = 0; i < elements.length; i++) {
-			var textNode = elements[i].children[0];
-			textNode.innerText = elements[i].dataset.ellipsis;
-			if(textNode.clientHeight > elements[i].clientHeight) {
-				polyfillElement(elements[i]);	
-			}
-		}
-	}
+    function polyfillElement(element) {
+        var textNode = element.children[0];
+        while (textNode.clientHeight > element.clientHeight) {
+            textNode.innerText = textNode.innerText.replace(/\W*\s(\S)*$/, '...');
+        }
+    }
 
-	function add() {
-		if (typeof(document.body.style.webkitLineClamp) !== "undefined") {
-			return;
-		}
-		elements = document.querySelectorAll('[data-ellipsis]');
-		reapply();
-		window.addEventListener('resize', reapply);
-		window.addEventListener('orientationchange', reapply);
-	}
+    function reapply() {
+        for (var i = 0; i < elements.length; i++) {
+            var textNode = elements[i].children[0];
+            textNode.innerText = elements[i].dataset.ellipsis;
+            if (textNode.clientHeight > elements[i].clientHeight) {
+                polyfillElement(elements[i]);
+            }
+        }
+    }
 
-	function remove() {
-		window.removeEventListener('resize', reapply);
-		window.removeEventListener('orientationchange', reapply);
-		elements.forEach(function(element) {
-			var textNode = element.children[0];
-			textNode.innerText = element.dataset.ellipsis;
-		});
-	}
+    function add() {
+        if (typeof(document.body.style.webkitLineClamp) !== "undefined") {
+            return;
+        }
+        elements = document.querySelectorAll('[data-ellipsis]');
+        reapply();
+        window.addEventListener('resize', reapply);
+        window.addEventListener('orientationchange', reapply);
+    }
 
-	var polyfill = {
-		add: add,
-		reapply: reapply,
-		remove: remove
-	};
+    function remove() {
+        window.removeEventListener('resize', reapply);
+        window.removeEventListener('orientationchange', reapply);
+        elements.forEach(function(element) {
+            var textNode = element.children[0];
+            textNode.innerText = element.dataset.ellipsis;
+        });
+    }
 
-	window.lineClamp = window.lineClamp || {
-		polyfillElement: polyfillElement,
-		polyfill: polyfill
-	};
+    var polyfill = {
+        add: add,
+        reapply: reapply,
+        remove: remove
+    };
+
+    window.lineClamp = window.lineClamp || {
+        polyfillElement: polyfillElement,
+        polyfill: polyfill
+    };
 
 }());
 
